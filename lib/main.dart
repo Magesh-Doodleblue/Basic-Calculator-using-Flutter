ef1f2f6845f2eb476dfbc4ff37ef766111876b7e
mageshcoder <codermagesh@gmail.com>
2023-06-08 13:56:34 +0530

    Final Commit - All fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 // // ignore_for_file: unnecessary_null_comparison, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:awesome_notifications/awesome_notifications.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
